49741ed35200befd0a7fd8deeb4abe984e43705c
DiscreetIntellectual <terentev-daniil@yandex.ru>
2018-12-14 22:27:27 +0300

    Задачи из 9 урока

--- src/lesson9/task1/Matrix.kt
+++ src/lesson9/task1/Matrix.kt
@@ -38,32 +38,62 @@ interface Matrix<E> {
  * height = высота, width = ширина, e = чем заполнить элементы.
  * Бросить исключение IllegalArgumentException, если height или width <= 0.
  */
-fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> = TODO()
+fun <E> createMatrix(height: Int, width: Int, e: E): Matrix<E> {
+    if (height <= 0 || width <= 0) throw IllegalArgumentException()
+    return MatrixImpl(height, width, e)
+}
 
 /**
  * Средняя сложность
  *
  * Реализация интерфейса "матрица"
  */
-class MatrixImpl<E> : Matrix<E> {
-    override val height: Int = TODO()
+class MatrixImpl<E>(override val height: Int, override val width: Int, e: E) : Matrix<E> {
 
-    override val width: Int = TODO()
+    private val list = mutableListOf<MutableList<E>>()
 
-    override fun get(row: Int, column: Int): E  = TODO()
+    init {
+        for (i in 0 until height)
+            list.add(MutableList(width) { e })
+    }
 
-    override fun get(cell: Cell): E  = TODO()
+    override fun get(row: Int, column: Int): E  = list[row][column]
+
+    override fun get(cell: Cell): E  = get(cell.row, cell.column)
 
     override fun set(row: Int, column: Int, value: E) {
-        TODO()
+        list[row][column] = value
     }
 
     override fun set(cell: Cell, value: E) {
-        TODO()
+        set(cell.row, cell.column, value)
     }
 
-    override fun equals(other: Any?) = TODO()
+    override fun equals(other: Any?): Boolean {
+        if (other is MatrixImpl<*>) {
+            if (height != other.height || width != other.width) return false
+            for (i in 0 until height)
+                for (j in 0 until width)
+                    if (list[i][j] != other[i, j]) return false
+            return true
+        }
+        else return false
+    }
 
-    override fun toString(): String = TODO()
+    override fun toString(): String {
+        val str = StringBuilder()
+        for (i in 0 until height)
+            for (j in 0 until width)
+                str.append(list[i][j])
+        return str.toString()
+    }
+
+    override fun hashCode(): Int {
+        var result = 5
+        result = result * 31 + height
+        result = result * 31 + width
+        result = result * 31 + list.hashCode()
+        return result
+    }
 }
 
